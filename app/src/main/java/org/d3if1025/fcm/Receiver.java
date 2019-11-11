3e27db281ef5feffc06d560f01f524c07d374c5c
Yusri Yusron <yusriyusron0530@gmail.com>
2019-11-11 15:19:10 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if1025/fcm/Receiver.java
@@ -0,0 +1,19 @@
+package org.d3if1025.fcm;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class Receiver extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if (remoteMessage.getNotification() != null){
+            Log.d("app",remoteMessage.getNotification().getBody());
+        }
+    }
+}
